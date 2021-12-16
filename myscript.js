8dc62c80f3b19a196bc191fe7c6ec743bbe5ee10
lorenzo-guerrini <lorenzoguerrini8@live.it>
2021-12-16 09:12:52 +0100

    Cambiato nome currentItemIndex

--- myscript.js
+++ myscript.js
@@ -24,6 +24,7 @@ const text = [
 
 // Inserisce le immagini nell'images-list a dx
 for (let i = 0; i < items.length; i++) {
+
     const image = document.createElement("div");
     image.classList.add("selector-image-" + i);
 
@@ -45,62 +46,62 @@ document.querySelector(".showing-image-title").innerHTML = title[0];
 document.querySelector(".showing-image-text").innerHTML = text[0];
 
 //Contatore item
-let currentItem = 0;
+let currentItemIndex = 0;
 
 // Funzionamento arrow up
 document.querySelector(".arrow.up").addEventListener("click", function () {
 
     //Gestisce l'item da deselezionare a dx
-    const oldItem = document.querySelector(".selector-image-" + currentItem);
+    const oldItem = document.querySelector(".selector-image-" + currentItemIndex);
 
     oldItem.classList.remove("selected");
     oldItem.classList.add("unselected");
 
     //Gestisce il contatore diminuendolo
-    currentItem--;
-    if (currentItem < 0) {
-        currentItem = items.length - 1;
+    currentItemIndex--;
+    if (currentItemIndex < 0) {
+        currentItemIndex = items.length - 1;
     }
 
     //Gestisce l'item da selezionare a dx
-    const newItem = document.querySelector(".selector-image-" + currentItem);
+    const newItem = document.querySelector(".selector-image-" + currentItemIndex);
 
     newItem.classList.remove("unselected");
     newItem.classList.add("selected");
 
     //Gestisce il comportamento degli elementi a sx
-    document.querySelector(".showing-image").style.backgroundImage = "url(" + items[currentItem] + ")";
+    document.querySelector(".showing-image").style.backgroundImage = "url(" + items[currentItemIndex] + ")";
 
-    document.querySelector(".showing-image-title").innerHTML = title[currentItem];
+    document.querySelector(".showing-image-title").innerHTML = title[currentItemIndex];
 
-    document.querySelector(".showing-image-text").innerHTML = text[currentItem];
+    document.querySelector(".showing-image-text").innerHTML = text[currentItemIndex];
 });
 
 // Funzionamento arrow down
 document.querySelector(".arrow.down").addEventListener("click", function () {
 
     //Gestisce l'item da deselezionare a dx
-    const oldItem = document.querySelector(".selector-image-" + currentItem);
+    const oldItem = document.querySelector(".selector-image-" + currentItemIndex);
 
     oldItem.classList.remove("selected");
     oldItem.classList.add("unselected");
 
     //Gestisce il contatore aumentandolo
-    currentItem++;
-    if (currentItem > items.length - 1) {
-        currentItem = 0;
+    currentItemIndex++;
+    if (currentItemIndex > items.length - 1) {
+        currentItemIndex = 0;
     }
 
     //Gestisce l'item da selezionare a dx
-    const newItem = document.querySelector(".selector-image-" + currentItem);
+    const newItem = document.querySelector(".selector-image-" + currentItemIndex);
 
     newItem.classList.remove("unselected");
     newItem.classList.add("selected");
 
     //Gestisce il comportamento degli elementi a sx
-    document.querySelector(".showing-image").style.backgroundImage = "url(" + items[currentItem] + ")";
+    document.querySelector(".showing-image").style.backgroundImage = "url(" + items[currentItemIndex] + ")";
 
-    document.querySelector(".showing-image-title").innerHTML = title[currentItem];
+    document.querySelector(".showing-image-title").innerHTML = title[currentItemIndex];
 
-    document.querySelector(".showing-image-text").innerHTML = text[currentItem];
+    document.querySelector(".showing-image-text").innerHTML = text[currentItemIndex];
 });
